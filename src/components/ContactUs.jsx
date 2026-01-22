47807b406e5d36998ff5a5c910a0e7d6ada0f911
NwayOoPyaeZo <nwayoopyaezo@gmail.com>
2026-01-22 08:15:45 +0700

    fix the icon and name center problems

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -16,20 +16,20 @@ const ContactUs = () => {
 
             <div className="relative w-full flex justify-center px-4 mt-10">
                 <form className="w-full max-w-[853px] flex flex-col md:flex-row gap-4 md:gap-6 lg:gap-10">
-                    <label className="flex w-full md:w-[298px] h-[64px] items-center rounded-[12px] border-2 border-[#32BDFF] px-[12px] bg-white">
-                        <img src="src/assets/elements/person.svg" alt="person" className="ml-3 h-5 w-5 flex-shrink-0" />
+                    <label className="flex w-full md:w-[298px] h-[64px] items-center justify-center rounded-[12px] border-2 border-[#32BDFF] px-[12px] bg-white">
+                        <img src="src/assets/elements/person.svg" alt="person" className="relative lg:left-5 md:left-5 h-5 w-5 flex-shrink-0" />
                         <input
                             type="text"
                             placeholder="Enter Your First Name"
-                            className="w-full bg-transparent font-semibold text-[16px] text-[#B0B0B0] placeholder:text-[#B0B0B0] outline-none px-3"
+                            className="w-full bg-transparent font-semibold text-[16px] text-[#B0B0B0] placeholder:text-[#B0B0B0] outline-none px-3 text-center placeholder:text-center"
                         />
                     </label>
-                    <label className="flex w-full md:w-[298px] h-[64px] items-center rounded-[12px] border-2 border-[#32BDFF] px-[12px] bg-white">
-                        <img src="src/assets/elements/email.svg" alt="email" className="ml-3 h-5 w-5 flex-shrink-0" />
+                    <label className="flex w-full md:w-[298px] h-[64px] items-center justify-center rounded-[12px] border-2 border-[#32BDFF] px-[12px] bg-white">
+                        <img src="src/assets/elements/email.svg" alt="email" className="relative lg:left-4 md:left-5 h-5 w-5 flex-shrink-0" />
                         <input
                             type="email"
                             placeholder="Enter Your Email Address"
-                            className="w-full bg-transparent font-semibold text-[16px] text-[#B0B0B0] placeholder:text-[#B0B0B0] outline-none px-3"
+                            className="w-full bg-transparent font-semibold text-[16px] text-[#B0B0B0] placeholder:text-[#B0B0B0] outline-none px-3 text-center placeholder:text-center"
                         />
                     </label>
                     <button className="w-full md:w-[298px] h-[64px] rounded-[12px] bg-[#00A4F4] px-[24px] py-[20px] flex items-center justify-center gap-2 shadow-[-8px_8px_28px_rgba(0,0,0,0.06)] text-[18px] md:text-[20px] lg:text-[24px] font-bold text-white">
